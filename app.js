45ac75c672b7c68258068fef3ad0dd46d798dfec
shanbhagpratik19 <shanbhag.pratik19@gmail.com>
2026-06-07 02:28:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,16 +21,13 @@ async function loadMatches() {
             const match = doc.data();
 
             html += `
-                <div class="match-card"
-                     onclick="openMatch('${doc.id}')">
-
-                    <h3>${match.team1} vs ${match.team2}</h3>
-
-                    <p>${match.format}</p>
-
-                    <p>${match.venue}</p>
-
-                </div>
+                <a href="match.html?id=${doc.id}" style="text-decoration:none;color:inherit;">
+                    <div class="match-card">
+                        <h3>${match.team1} vs ${match.team2}</h3>
+                        <p>${match.format}</p>
+                        <p>${match.venue}</p>
+                    </div>
+                </a>
             `;
         });
 
@@ -43,11 +40,5 @@ async function loadMatches() {
         container.innerHTML = "Unable to load matches.";
     }
 }
-
-window.openMatch = function(matchId) {
-
-    window.location.href =
-        "match.html?id=" + matchId;
-};
 
 loadMatches();
