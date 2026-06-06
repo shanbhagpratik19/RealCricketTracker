42309a0de115762e9ac0a7a36b42bbd8ab91acdb
shanbhagpratik19 <shanbhag.pratik19@gmail.com>
2026-06-07 01:02:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
+import { db, collection, addDoc } from "./firestore.js";
+
 console.log("RealCricketTracker Loaded");
-console.log("Firebase configuration file created");
-function saveMatch() {
+
+window.saveMatch = async function () {
 
     const team1 = document.getElementById("team1")?.value;
     const team2 = document.getElementById("team2")?.value;
     const format = document.getElementById("format")?.value;
     const venue = document.getElementById("venue")?.value;
 
-    alert(
-        `Match Saved
+    try {
 
-${team1} vs ${team2}
-${format}
-${venue}`
-    );
-}
+        await addDoc(collection(db, "matches"), {
+            team1,
+            team2,
+            format,
+            venue,
+            createdAt: new Date().toISOString()
+        });
+
+        alert("Match saved to Firestore!");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert("Error saving match.");
+    }
+};
