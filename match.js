8343c9b093d32aa1a4ec06c279068eaab7cd1ff6
shanbhagpratik19 <shanbhag.pratik19@gmail.com>
2026-06-07 02:04:33 +0530

    Create match.js

---
+++ match.js
@@ -0,0 +1,22 @@
+const container =
+    document.getElementById("matchDetails");
+
+container.innerHTML = `
+    <div class="match-card">
+        <h2>Match Details Coming Soon</h2>
+
+        <p>
+            This page will display:
+        </p>
+
+        <ul>
+            <li>Toss</li>
+            <li>Venue</li>
+            <li>Result</li>
+            <li>ODI Phases</li>
+            <li>Test Innings</li>
+            <li>T20 Phases</li>
+            <li>Match Report</li>
+        </ul>
+    </div>
+`;
