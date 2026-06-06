b2268c4a2e5a1fc7a6a6969eb35a4cbdfabef8ca
shanbhagpratik19 <shanbhag.pratik19@gmail.com>
2026-06-07 01:00:38 +0530

    Create firestore.js

---
+++ firestore.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.14.0/firebase-app.js";
+
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBnR-3fcC5B2CoZNR9GECjbdGmeLhCXn-c",
+  authDomain: "realcrickettracker.firebaseapp.com",
+  projectId: "realcrickettracker",
+  storageBucket: "realcrickettracker.firebasestorage.app",
+  messagingSenderId: "291544787588",
+  appId: "1:291544787588:web:ab62f2e61687b33cfeb0d1"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db, collection, addDoc };
