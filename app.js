54d16eb245e1ea8229d661dd265cbdcdb87077dd
shanbhagpratik19 <shanbhag.pratik19@gmail.com>
2026-06-07 00:53:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
 console.log("RealCricketTracker Loaded");
 console.log("Firebase configuration file created");
+function saveMatch() {
+
+    const team1 = document.getElementById("team1")?.value;
+    const team2 = document.getElementById("team2")?.value;
+    const format = document.getElementById("format")?.value;
+    const venue = document.getElementById("venue")?.value;
+
+    alert(
+        `Match Saved
+
+${team1} vs ${team2}
+${format}
+${venue}`
+    );
+}
